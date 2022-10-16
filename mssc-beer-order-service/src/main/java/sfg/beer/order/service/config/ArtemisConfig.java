aba69ee1c830da93356bcca235a8776dcc6be749
oktay.cekmez <oktay.cekmez@i2i-systems.com>
2022-10-16 12:44:28 +0300

    minor changes

--- mssc-beer-order-service/src/main/java/sfg/beer/order/service/config/ArtemisConfig.java
+++ mssc-beer-order-service/src/main/java/sfg/beer/order/service/config/ArtemisConfig.java
@@ -1,6 +1,9 @@
 package sfg.beer.order.service.config;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.activemq.artemis.api.core.TransportConfiguration;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyAcceptorFactory;
+import org.apache.activemq.artemis.core.remoting.impl.netty.NettyConnectorFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisConfigurationCustomizer;
 import org.springframework.context.annotation.Configuration;
@@ -17,12 +20,15 @@ public class ArtemisConfig implements ArtemisConfigurationCustomizer {
 
     @Override
     public void customize(org.apache.activemq.artemis.core.config.Configuration configuration) {
+        configuration.addConnectorConfiguration("nettyConnector", new TransportConfiguration(NettyConnectorFactory.class.getName()));
+        configuration.addAcceptorConfiguration(new TransportConfiguration(NettyAcceptorFactory.class.getName()));
+
         // Allow Artemis to accept tcp connections (Default port localhost:61616)
-        final String uri = "tcp://" + host + ":" + port;
+      /*  final String uri = "tcp://" + host + ":" + port;
         try {
             configuration.addAcceptorConfiguration("netty", uri);
         } catch (Exception e) {
             log.error("Cannot set host/port for Artemis Server", e);
-        }
+        }*/
     }
 }
